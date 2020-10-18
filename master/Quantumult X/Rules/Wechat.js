002630e87774b256bcb32036d606bbba21baec03
eghawk <66006679+eghawk@users.noreply.github.com>
2020-10-18 22:52:57 +0800

    Update Wechat.js

--- master/Quantumult X/Rules/Wechat.js
+++ master/Quantumult X/Rules/Wechat.js
@@ -2,10 +2,10 @@
 Remove the WeChat public account bottom ad
 by Choler
 QX:
-^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/eghawk/Library/Library/master/Quantumult%20X/Rules/Wechat.js
 
 Surge4：
-http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/eghawk/Library/Library/master/Quantumult%20X/Rules/Wechat.js
 
 Surge & QX MITM = mp.weixin.qq.com,
 */
